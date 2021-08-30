d17a94a4e224baaa219c1c83a08d4043a33c5501
最燃系坚果 <1263067775@qq.com>
2021-08-31 02:03:09 +0800

    功能分流

--- B站视频评论爬虫/BilibiliVideoCommentsSpider.py
+++ B站视频评论爬虫/BilibiliVideoCommentsSpider.py
@@ -59,22 +59,7 @@ def Out2File(dict):
                 print("out2File error")
         print('当前页面保存完成')
 
-if __name__ == '__main__':
-    e=0
-    page=1
-    while e == 0 :
-        url = "https://api.bilibili.com/x/v2/reply?pn="+ str(page)+"&type=1&oid=17784172&sort=2" 	
-        try:
-            print()
-            content=get_content(url)
-            print("page:",page)
-            Out2File(content)
-            page=page+1
-            # 为了降低被封ip的风险，每爬20页便歇5秒。
-            if page%10 == 0:
-                time.sleep(5)
-        except:
-            e=1
+
     path_txt='BiliBili评论.txt'
     f = open(path_txt,'r',encoding='UTF-8').read()
 
